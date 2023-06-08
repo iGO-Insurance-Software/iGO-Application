e290a8641e3b0cf530d94c6e5d9921afeda541d6
DryRains <dryrain_@naver.com>
2023-06-09 00:27:23 +0900

    add JConnector Library

--- src/Dao/Dao.java
+++ src/Dao/Dao.java
@@ -7,12 +7,13 @@ public class Dao {
     private Connection connect = null;
     private Statement statement = null;
     private ResultSet resultSet = null;
+    private String password = "";
 
     public void connect() throws Exception {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connect = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/insurance?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true", "root", "8817");
+                    "jdbc:mysql://localhost:3306/insurance?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true", "root", password);
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
